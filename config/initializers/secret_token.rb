b569473f3318970769bed105dde645e44b88818a
Mailo Svetel <mailo@kazimir.(none)>
2010-12-20 01:00:37 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorHo::Application.config.secret_token = 'cbc31365c3ac5ac8cffafde07ba73277977649af5bc778a7c186bca82903c4f7f79e670b1f366a9d563a4a1a4e038db857e9a01d21d9aaa7d070ff8ba90e9ee4'
